7d05970a675720ef7f7e96eff86ef47b419df044
Yearis <khush8751@gmail.com>
2025-07-22 23:44:37 +0530

    relocated main class to root folder
    And some other small changes
    Work-In-Progress right now.

--- src/library/models/Admin.java
+++ src/library/models/Admin.java
@@ -1,13 +1,5 @@
 package library.models;
 
 public class Admin {
-    String adminID;
-    String password;
 
-    public void
-    /*
-        if (!contentRating.matches("G|PG-13|R")) {
-            throw new IllegalArgumentException("Invalid rating! Use G, PG-13, or R.");
-        }
-     */
 }
--- src/library/services/Main.java
+++
@@ -1,5 +0,0 @@
- package library.services;
-
-public class Main {
-
-}
